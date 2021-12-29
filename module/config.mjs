a7de893fcce49dc4b6f1c86724d0b349e5106ae3
Professor Geeksworth <geekswordsman@gmail.com>
2021-12-29 10:48:34 -0500

    Resolves #2

--- module/config.mjs
+++ module/config.mjs
@@ -2,7 +2,34 @@ export const SWEETNOTHINGS = {};
 
 SWEETNOTHINGS.ID = "sweetnothings";
 SWEETNOTHINGS.MODULE_PATH = `modules/${SWEETNOTHINGS.ID}/`;
+
+SWEETNOTHINGS.SETTINGS = {
+    "DEFAULT_CHATMODE": {
+        config: false,
+        default: 'other',
+        scope: 'client',
+        type: String,
+        choices: {
+            "ic": "SWEETNOTHINGS.DIALOG.MODES.IC",
+            "ooc": "SWEETNOTHINGS.DIALOG.MODES.OOC",
+            "emote": "SWEETNOTHINGS.DIALOG.MODES.EMOTE",
+            "other": "SWEETNOTHINGS.DIALOG.MODES.OTHER"
+        }
+    },
+    "DEFAULT_DIALOG": {
+        config: false,
+        default: 'whisper',
+        scope: 'client',
+        type: String,
+        choices: {
+            "whisper": "SWEETNOTHINGS.DIALOG.WHISPER",
+            "chat": "SWEETNOTHINGS.DIALOG.CHAT"
+        }
+    }
+}
+
 SWEETNOTHINGS.TEMPLATE_PATH = `${SWEETNOTHINGS.MODULE_PATH}/templates/`;
 SWEETNOTHINGS.TEMPLATES = {
+    CONFIG: `${SWEETNOTHINGS.TEMPLATE_PATH}sweetNothingsConfig.hbs`,
     DIALOG: `${SWEETNOTHINGS.TEMPLATE_PATH}sweetNothingsDialog.hbs`
 }
